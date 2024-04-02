85bab81be47ad807a6e7123049f8b5e2f136f2da
yusuke <kushida.yusuke@formzu.co.jp>
2024-04-02 12:30:55 +0900

    -nコマンドを追加

--- cat.c
+++ cat.c
@@ -1,24 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int main(int argc, char **argv) {
   if (argc < 2) {
     fprintf(stderr, "引数の数が異常です\n");
     exit(1);
   }
 
-  for(int i = 1; i < argc; i++) {
-    FILE *file;
-    file = fopen(argv[i], "r");
-    if(file == NULL) {
-      fprintf(stderr, "ファイルを開くことができませんでした\n");
-      exit(1);
+  if(strcmp(argv[1], "-n") == 0) {
+    if(argc < 3) {
+      fprintf(stderr, "引数の数が異常です\n");
+    }  
+    
+    //現在の行数をカウントする
+    int counter = 1;
+    for(int i = 2; i < argc; i++) {
+      FILE *file;
+      file = fopen(argv[i], "r");
+      if(file == NULL) {
+        fprintf(stderr, "ファイルを開くことができませんでした\n");
+	exit(1);
+      }
+      char buf[200];
+      while(fgets(buf, 200, file) != NULL) {
+        printf("%d", counter);
+	counter += 1;
+        printf("%s", buf);
+      }
+      fclose(file);
     }
-    //テキストファイルの内容を一行ずつbufに読み込んで標準出力する
-    char buf[200];
-    while(fgets(buf, 200, file) != NULL) {
-      printf("%s", buf);
-    }
-    fclose(file);
   }
+
+  else {
+    for(int i = 1; i < argc; i++) {
+      FILE *file;
+      file = fopen(argv[i], "r");
+      if(file == NULL) {
+        fprintf(stderr, "ファイルを開くことができませんでした\n");
+        exit(1);
+      }
+      //テキストファイルの内容を一行ずつbufに読み込んで標準出力する
+      char buf[200];
+      while(fgets(buf, 200, file) != NULL) {
+        printf("%s", buf);
+      }
+      fclose(file);
+    }
+  }
+
   return 0;
+
 }
+
+
