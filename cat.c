fee042cdd7942f89bacf9fcccdd0b23908f56b09
yusuke <kushida.yusuke@formzu.co.jp>
2024-04-02 14:30:30 +0900

    -bオプションを追加

--- cat.c
+++ cat.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 int main(int argc, char **argv) {
   if (argc < 2) {
     fprintf(stderr, "引数の数が異常です\n");
@@ -11,7 +12,7 @@ int main(int argc, char **argv) {
     if(argc < 3) {
       fprintf(stderr, "引数の数が異常です\n");
     }  
-    
+  
     //現在の行数をカウントする
     int counter = 1;
     for(int i = 2; i < argc; i++) {
@@ -30,7 +31,34 @@ int main(int argc, char **argv) {
       fclose(file);
     }
   }
-
+  
+  else if(strcmp(argv[1], "-b") == 0) {
+    if(argc < 3) {
+      fprintf(stderr, "引数の数が異常です\n");
+      exit(1);  
+    }
+    int counter_b = 1;
+    for(int i = 2; i < argc; i++) {
+      FILE *file;
+      file = fopen(argv[i], "r");
+      if(file == NULL) {
+        fprintf(stderr, "ファイルを開くことができませんでした\n");
+	exit(1);
+      }
+      char buf[200];
+      while(fgets(buf, 200, file) != NULL) {
+	if(buf[0] != '\n') {
+	//if(strlen(buf) != 0) {
+	//if(isspace(buf[0]) == 0) {
+          printf("%d", counter_b);
+	  counter_b += 1;
+	}
+        printf("%s", buf);	
+      }
+      fclose(file);
+    }
+  }
+    
   else {
     for(int i = 1; i < argc; i++) {
       FILE *file;
