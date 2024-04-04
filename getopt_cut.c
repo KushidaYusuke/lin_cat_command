821e828dc9247a90227ec37ec9b37faa8cca7438
yusuke <kushida.yusuke@formzu.co.jp>
2024-04-04 10:09:47 +0900

    cオプションの正規表現を用いた処理

--- getopt_cut.c
+++ getopt_cut.c
@@ -4,6 +4,7 @@
 #include<ctype.h>
 #include<stdbool.h>
 #include<unistd.h>
+#include<regex.h>
 int bopt = 0;
 int copt = 0;
 int fopt = 0;
@@ -14,8 +15,94 @@ char *cparam = NULL;
 char *fparam = NULL;
 char *dparam = NULL;
 
+//正規表現を用いた判定のための変数
+//char checkstring[] = "2-9";
+const char regex_1[] = "^([0-9]+)-$";
+const char regex_2[] = "^-([0-9]+)$";
+const char regex_3[] = "^([0-9]+)-([0-9]+)$";
+regex_t regexBuffer;
+regmatch_t match[4];
+int size;
+char result[128];
+
+const int INF = 10000;
+
+int regex_check(char *checkstring) {
+  if(regcomp(&regexBuffer, regex_1, REG_EXTENDED | REG_NEWLINE) != 0) {
+    return -1;
+    
+  }
+  size = sizeof(match)/sizeof(regmatch_t);
+  if(regexec(&regexBuffer, checkstring, size, match, 0) == 0) {
+    return 1;
+  }
+  regfree(&regexBuffer);
+
+  if(regcomp(&regexBuffer, regex_2, REG_EXTENDED | REG_NEWLINE) != 0) {
+    return -1;
+  }
+  if(regexec(&regexBuffer, checkstring, size, match, 0) == 0) {
+    return 2;
+  }
+  regfree(&regexBuffer);
+ 
+  if(regcomp(&regexBuffer, regex_3, REG_EXTENDED | REG_NEWLINE) != 0) {
+    return -1;
+  }
+  if(regexec(&regexBuffer, checkstring, size, match, 0) == 0) {
+    return 3;
+  }
+  regfree(&regexBuffer);
+}
+
+
 void cut_command(FILE *file) {
-  if(fparam && dparam) {
+  if(copt) {
+    int lower_bound = 0;
+    int upper_bound = INF;
+
+    int match_num = regex_check(cparam);
+    //c 5- のような場合
+    if(match_num == 1) {
+      sscanf(cparam, "%d-", &lower_bound);
+      printf("パターン1");
+      lower_bound -= 1; //0-index
+    }
+    // c -7のような場合
+    if(match_num == 2) {
+      sscanf(cparam, "-%d", &upper_bound);
+      printf("パターン2");
+      upper_bound -= 1;
+    }
+    // c 2-7のような場合
+    if(match_num == 3) {
+      sscanf(cparam, "%d-%d", &lower_bound, &upper_bound);
+      printf("パターン3");
+      upper_bound -= 1;
+      lower_bound -= 1;
+    }
+    //いずれかの正規表現にマッチした場合の処理
+    if(match_num == 1 || match_num == 2 || match_num == 3) {
+      int now_index = 0; //現在の行頭から0-indexで何番目か
+      int c;
+      while((c = fgetc(file)) != EOF) {
+        //改行の場合
+        if(c == '\n') {
+          putchar(c);
+	  now_index = 0;
+	  continue;
+        }
+        else if((now_index >= lower_bound) && (now_index <= upper_bound)) {
+          putchar(c);
+        }
+        now_index += 1;
+      }
+      fclose(file); 
+    }
+
+  }
+
+  if(fopt && dopt) {
     int cut_field = atoi(fparam);
     char cut_letter = dparam[0]; //-dで指定した区切り文字：
     cut_field -= 1;
