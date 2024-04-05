bb1c0d59727d6e86cb2461fbf05778ba0c3af517
yusuke <kushida.yusuke@formzu.co.jp>
2024-04-05 14:17:33 +0900

    ファイル名を変更

--- cut.c
+++ cut.c
@@ -3,23 +3,120 @@
 #include<string.h>
 #include<ctype.h>
 #include<stdbool.h>
+#include<unistd.h>
+#include<regex.h>
+int bopt = 0;
+int copt = 0;
+int fopt = 0;
+int dopt = 0;
 
-int main(int argc, char **argv) {
-  if(argc < 3) {
-    fprintf(stderr, "引数が足りていません");
-    exit(1);
+char *bparam = NULL;
+char *cparam = NULL;
+char *fparam = NULL;
+char *dparam = NULL;
+
+//正規表現を用いた判定のための変数
+//char checkstring[] = "2-9";
+const char regex_1[] = "^([0-9]+)-$";
+const char regex_2[] = "^-([0-9]+)$";
+const char regex_3[] = "^([0-9]+)-([0-9]+)$";
+regex_t regexBuffer;
+regmatch_t match[4];
+int size;
+char result[128];
+
+#define INF 10000
+int token_list[INF];
+
+char *delim = ",";
+int delim_count = 0;
+
+
+int match_num = 0;
+
+int lower_bound = 0;
+int upper_bound = INF;
+
+
+//正規表現の判定をする関数
+//オプションの引数が%d-, %d-%d, -%dという形をしているときにTrue, それ以外の場合にfalseを返す
+//match_num=1: 引数は %d-型	
+//match_num=2: 引数は %d-%d型
+//match_num=3: 引数は -%d型
+bool regex_check(char *checkstring) {
+  if(regcomp(&regexBuffer, regex_1, REG_EXTENDED | REG_NEWLINE) != 0) {
+    return false;
+    
   }
+  size = sizeof(match)/sizeof(regmatch_t);
+  if(regexec(&regexBuffer, checkstring, size, match, 0) == 0) {
+    regfree(&regexBuffer);
+    match_num = 1;
+  }
+  if(regcomp(&regexBuffer, regex_2, REG_EXTENDED | REG_NEWLINE) != 0) {
+    return false;
+  }
+  if(regexec(&regexBuffer, checkstring, size, match, 0) == 0) {
+    regfree(&regexBuffer);
+    match_num = 2;
+  }
+  if(regcomp(&regexBuffer, regex_3, REG_EXTENDED | REG_NEWLINE) != 0) {
+    return false;
+  }
+  if(regexec(&regexBuffer, checkstring, size, match, 0) == 0) {
+    regfree(&regexBuffer);
+    match_num = 3;
+  }
+  if(match_num == 1) {
+    sscanf(checkstring, "%d-", &lower_bound);
+    if(!bopt){lower_bound -= 1;} //0-index
+    upper_bound = INF;
+  }
+    // c -7のような場合
+  if(match_num == 2) {
+    sscanf(checkstring, "-%d", &upper_bound);
+    if(!bopt){upper_bound -= 1;}
+    lower_bound = 0;
+  }
+    // c 2-7のような場合
+  if(match_num == 3) {
+    sscanf(checkstring, "%d-%d", &lower_bound, &upper_bound);
+    if(!bopt) {
+      upper_bound -= 1;
+      lower_bound -= 1;
+    }
+  }
+  //いずれかの正規表現にマッチした場合
+  if(match_num == 1 | match_num == 2 | match_num == 3) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-  if(strcmp(argv[1], "-c") == 0) {
-    int cut_index = atoi(argv[2]);
-    cut_index -= 1; //indexは0スタート
-    for(int i = 3; i < argc; i++) {
-      FILE *file;
-      file = fopen(argv[i], "r");
-      if(file == NULL) {
-        fprintf(stderr, "ファイルを開くことができません");
-        exit(1);
-      }
+//オプションの引数が1,3,4という形をしている場合にそれぞれの数値をリストに入れて返す
+int *token_parse(char *param) {
+  char *token = strtok(param, delim);
+  while(token != NULL) {
+    if(!bopt) {token_list[delim_count] = atoi(token)-1;}
+    else  {token_list[delim_count] = atoi(token);}
+    delim_count += 1;
+    token = strtok(NULL, delim);
+  }
+  return token_list;
+}
+
+
+
+void cut_command(FILE *file) {
+  if(copt) {
+
+    bool is_regex_matched = regex_check(cparam);
+    //printf("match_numは%d", match_num);
+    //いずれかの正規表現にマッチした場合の処理
+    if(is_regex_matched) {
+      //printf("debug\n");
       int now_index = 0; //現在の行頭から0-indexで何番目か
       int c;
       while((c = fgetc(file)) != EOF) {
@@ -29,78 +126,250 @@ int main(int argc, char **argv) {
 	  now_index = 0;
 	  continue;
         }
-        else if(now_index == cut_index) {
+        else if((now_index >= lower_bound) && (now_index <= upper_bound)) {
           putchar(c);
         }
         now_index += 1;
       }
       fclose(file); 
     }
-  }
-
-  if(strcmp(argv[1], "-b") == 0) {
-    int cut_byte = atoi(argv[2]);
-    for(int i = 3; i < argc; i++) {
-      FILE *file;
-      file = fopen(argv[i], "r");
-      if(file == NULL) {
-        fprintf(stderr, "ファイルを開くことができません");
-        exit(1);
+    //	-c 2,3,4のような形でオプションが与えられる場合の処理
+    else {       
+      int *token_list_c = token_parse(cparam);
+      int now_index = 0; //現在の行頭から0-indexで何番目か
+      int c;
+      while((c = fgetc(file)) != EOF) {
+        //改行の場合
+        if(c == '\n') {
+          putchar(c);
+	  now_index = 0;
+	  continue;
+        }
+        else {
+          for(int i = 0; i < delim_count; i++) {
+	    int cut_index = token_list_c[i];
+	    if(now_index == cut_index) {
+	      putchar(c);
+	    }
+	  }
+        }
+        now_index += 1;
       }
-      int now_byte = 0; //先頭から何バイトか
+      fclose(file); 
+      }
+    }
+
+
+  if(fopt) {
+    //int cut_field = atoi(fparam);
+    //cut_field -= 1;
+    
+    char cut_letter;
+    if(dopt) {
+      cut_letter = dparam[0]; //-dで指定した区切り文字：    
+    }
+    else {
+      cut_letter = '\t';
+    }
+    bool is_regex_matched_f = regex_check(fparam);
+    if(is_regex_matched_f) {
+      bool is_first_delim = true; //初めの区切り文字であるか判定
+      int now_field = 0;
+    
+      int c;      
+      while((c = fgetc(file)) != EOF) {
+	
+        //改行の場合
+        if(c == '\n') {
+          putchar(c);
+	  now_field = 0;
+	  is_first_delim = true;
+	  continue;
+        }
+        if(c == cut_letter) {
+          now_field += 1;
+	//  continue;
+        }
+      
+        if((now_field >= lower_bound) && (now_field <= upper_bound)) {
+          if(c != cut_letter) {
+	    is_first_delim = false;
+	  }
+	  if(!is_first_delim) {
+            putchar(c);
+	    }
+
+	}
+      }
+    }
+    //-f 2,3,4 -d ","のような形でオプションが与えられた場合の処理
+    else {
+      int *token_list_d = token_parse(fparam);
+      
+      bool is_first_delim = true; //初めの区切り文字であるか判定
+      int now_field = 0;
+    
+      int c;      
+      while((c = fgetc(file)) != EOF) {
+	
+        //改行の場合
+        if(c == '\n') {
+          putchar(c);
+	  now_field = 0;
+	  is_first_delim = true;
+	  continue;
+        }
+        if(c == cut_letter) {
+          now_field += 1;
+	//  continue;
+        }
+        
+	for(int i = 0; i < delim_count; i++) {  
+          int cut_field = token_list_d[i];
+	  if(now_field == cut_field) {
+            if(c != cut_letter) {
+	      is_first_delim = false;
+	    }
+	    if(!is_first_delim) {
+              putchar(c);
+	     }  
+	   }
+	}
+      }
+    }
+
+
+    }
+    
+
+
+  if(bopt) {
+    bool is_regex_matched_b = regex_check(bparam);
+    //printf("match_numは%d", match_num);
+    //いずれかの正規表現にマッチした場合の処理
+    if(is_regex_matched_b) {
+      int now_byte = 0; //現在の行頭から何バイト目か
       char c;
       while((c = fgetc(file)) != EOF) {
-	now_byte += (int)(sizeof(c)); //修正する
+	now_byte += (int)(sizeof(c));
+        //printf("デバッグ%d\n", now_byte);
 	//改行の場合
         if(c == '\n') {
           putchar(c);
 	  now_byte = 0;
 	  continue;
         }
-        else if(now_byte == cut_byte) {
+        else if((now_byte >= lower_bound) && (now_byte <= upper_bound)) {
           putchar(c);
         }
       }
       fclose(file); 
     }
-  }
-
-  //-fオプション(-dオプションで指定した区切り文字で区切られた項目の中で指定した項目数の項目を出力)
-  if((strcmp(argv[1], "-f") == 0) && (strcmp(argv[3], "-d") == 0)) {
-    int cut_field = atoi(argv[2]);
-    char cut_letter = argv[4][0]; //-dで指定した区切り文字：
-    cut_field -= 1;
-    //printf("cutfield%d", cut_field); //デバッグ
-    //printf("cutletter%c", cut_letter);//デバッグ
-    for(int i = 5; i < argc; i++) {
-      FILE *file;
-      file = fopen(argv[i], "r");
-      if(file == NULL) {
-        fprintf(stderr, "ファイルを開くことができません");
-        exit(1);
-      }
-      int now_field = 0; //先頭からタブ区切りで何フィールド目にいるか
+    //	-c 2,3,4のような形でオプションが与えられる場合の処理
+    else {       
+      int *token_list_b = token_parse(bparam);
+      int now_byte = 0; //現在の行頭から0-indexで何番目か
       char c;
-      if(c == cut_letter) {
-        now_field += 1;
+      for(int i = 0; i < delim_count; i++) {
+        //printf("デバッグ: token_list_cの%d番目の要素は%d", i, token_list_c[i]);
       }
       while((c = fgetc(file)) != EOF) {
-	
+        now_byte += (int)(sizeof(c));
 	//改行の場合
         if(c == '\n') {
           putchar(c);
-	  now_field = 0;
+	  now_byte = 0;
 	  continue;
         }
-	if(c == cut_letter) {
-	  now_field += 1;
-	  continue;
-	}
-        if(cut_field == now_field) {
-	  putchar(c);
-	}
+        else {
+          for(int i = 0; i < delim_count; i++) {
+	    int cut_byte = token_list_b[i];
+	    if(now_byte == cut_byte) {
+	      putchar(c);
+	    }
+	  }
+        }
       }
-      fclose(file);
+      fclose(file); 
+      }
     }
   }
+
+int main(int argc, char *argv[]) {
+  int opt;
+  while((opt = getopt(argc, argv, "b:c:f:d:")) != -1) {
+    switch(opt) {
+      case 'b':
+        bopt = 1;
+	bparam = optarg;
+	break;
+      case 'c':
+        copt = 1;
+	cparam = optarg;
+	break;
+      case 'f':
+        fopt = 1;
+	fparam = optarg;
+	break;
+      case 'd':
+        dopt = 1;
+	dparam = optarg;
+	break;
+    }
+  }
+  
+
+  //オプションの組み合わせが不適切の場合の処理
+  int pattern1 = (bopt | copt | fopt); //-b, -c, -fのいずれかのオプションが必須
+  int pattern2 = (dopt & !fopt);
+  if(pattern1 == 0 || pattern2) {
+    fprintf(stderr, "オプションの組み合わせが不適切です\n");
+    exit(1);
+  }
+  
+  if(argc == optind) {
+    while(1) {
+      cut_command(stdin); 
+    }
+  }
+  for(int i = optind; i < argc; i++) {
+    FILE *file;
+    file = fopen(argv[i], "r");
+    if(file == NULL) {
+      fprintf(stderr, "ファイルを開くことができません\n");
+      exit(1);
+    }
+    cut_command(file);
+  }
 }
+  //ファイルが指定されない場合
+  //(optind == argc) {
+    //ile(1) {
+      //intf("標準入力から受け取る");
+    //
+  //
+  //r(int i = optind; i < argc; i++) {
+    //LE *file;
+    //le = fopen(argv[i], "r");
+    //(file == NULL) {
+      //rintf(stderr, "ファイルを開くことができませんでした\n");
+     //xit(1);
+    //
+   //at_program(file);
+   //close(file);
+ //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
